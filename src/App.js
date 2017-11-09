83cf1ac3fa6db0dff3fdb44be9c48bf216989180
Лыков Владимир Евгеньевич <lykov@reagentum.ru>
2017-11-09 11:13:58 +0300

    Добавил приложение

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+import Header from "./Header";
+// TODO
+
+
+const PageWrapper = styled.section`
+  font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
+  padding-bottom: 24px;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <PageWrapper>
+        <Header />
+        <main>
+          {/* TODO */}
+        </main>
+        {/* <Footer /> */}
+      </PageWrapper>
+    );
+  }
+}
+
+export default App;
