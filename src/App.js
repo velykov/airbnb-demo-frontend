a7a19df1029827063ef9ef0f56e8dc678c562ed5
Лыков Владимир Евгеньевич <lykov@reagentum.ru>
2017-11-09 17:48:49 +0300

    + App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Explore from "./Explore";
 import Experiences from "./Experiences";
+import Homes from "./Homes";
+import Reservations from "./Reservations";
+import Destinations from "./Destinations";
 
 const PageWrapper = styled.section`
   font-family: Circular, Helvetica Neue, Helvetica, Arial, sans-serif;
@@ -18,6 +21,9 @@ class App extends Component {
         <main>
           <Explore />
           <Experiences />
+          <Homes />
+          <Reservations />
+          <Destinations />
           {/* TODO */}
         </main>
         <Footer />
