27acdfd61a5a2da6d6e32a69bb27b0971d7b9864
Vladimir Lykov <velykov@yandex.ru>
2017-11-11 11:41:50 +0300

    fix styles

--- src/commons/nextLink.js
+++ src/commons/nextLink.js
@@ -2,25 +2,29 @@ import React from "react";
 import styled from "styled-components";
 import arrow from "./arrow.svg";
 
-const Container = styled.div`
+const Container = styled.span`
   position: relative;
-  display: flex;
   align-items: center;
   justify-content: center;
+  display: none;
+
+  @media (min-width: 992px) {
+    display: flex;
+  }
 `;
 
 const NextLink = styled.a`
   position: absolute;
   right: 0px;
-  top: ${props => (props.top ? props.top : 0)}px;
-  bottom: ${props => (props.bottom ? props.bottom : 0)}px;
+  top: ${props => (props.top ? props.top : 0)}%;
+  bottom: ${props => (props.bottom ? props.bottom : 0)}%;
 
   width: 20px;
   height: 20px;
 
   color: #000;
   cursor: pointer;
-  outline: none;
+  
 
   stroke: 0.5px solid rgba(72, 72, 72, 0.2);
   stroke-align: inside;
@@ -30,7 +34,10 @@ const NextLink = styled.a`
   box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.16);
   box-sizing: border-box;
   transform: rotate(-90deg) scale(2, 2);
-`;
+  
+  &:focus{
+   outline:0;
+}`;
 
 export default function(props) {
   return (
