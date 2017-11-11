a0c5b64b14f1499238ea099646f4177fc88bfecc
Vladimir Lykov <velykov@yandex.ru>
2017-11-11 11:46:10 +0300

    rename component, add grid, fix styles for header section

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,16 +1,13 @@
 import React from "react";
+import { Grid, Row, Col } from "react-flexbox-grid";
 import styled from "styled-components";
 import logo from "./logo.svg";
 import search from "./search.svg";
+import arrow from "./../commons/arrow.svg";
+import { Img, Hr, Link } from "../commons/styled";
 
-import { Container, Column, Img, Hr, Link } from "../commons/styled";
-
-const Row = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  box-sizing: border-box;
-  margin-top: 16px;
+const Header = styled.header`
+  padding: 16px 0;
 `;
 
 const Logo = styled(Img)`
@@ -18,11 +15,24 @@ const Logo = styled(Img)`
   height: 32px;
 `;
 
+const Arrow = styled.a`
+    display: flex;
+    align-items: center;
+    align-self: center;
+    flex-direction: row;
+    
+    @media (max-width: 992px) {
+      &:after {
+        content: url("${arrow}") ;
+        width: 6px;
+        margin-left: 8px;
+        transform: rotate(0);
+        transform: translate(-50%);
+      }
+    }
+`;
+
 const Location = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  box-sizing: border-box;
   border: 1px solid rgba(72, 72, 72, 0.2);
   box-shadow: 0px 2px 4px rgba(72, 72, 72, 0.08);
   border-radius: 4px;
@@ -38,45 +48,47 @@ const LocationInput = styled.input`
   opacity: 0.64;
   font-family: Circular_Air-Book;
   padding-left: 54px;
-  outline: none;
   background: url('${search}') 16px center no-repeat;
+  &:focus {
+   outline: 0;
+  }
 `;
-
-const Offset = styled(Column)``;
 
 const Navigation = styled.nav`
   display: flex;
   justify-content: space-around;
-  align-items: center;
+
+  @media (max-width: 992px) {
+    display: none;
+  }
 `;
 
 export default function() {
   return (
-    <Container>
-      <Row>
-        <Column size={1}>
-          <Logo src={logo} alt="logo" />
-        </Column>
-        <Column size={5}>
-          <Location>
-            <LocationInput type="text" placeholder="Try “Miami”" />
-          </Location>
-        </Column>
-        <Offset size={2} />
-        <Column size={4}>
-          <Navigation>
-            <Link href="#">Become a host</Link>
-            <Link href="#">Help</Link>
-            <Link href="#">Sign up</Link>
-            <Link href="#">Log in</Link>
-          </Navigation>
-        </Column>
-      </Row>
-      <Row>
-        <Column size={12}>
-          <Hr />
-        </Column>
-      </Row>
-    </Container>
+    <Header>
+      <Grid>
+        <Row start="xs" middle="xs">
+          <Col xs={2} md={1} lg={1}>
+            <Arrow>
+              <Logo src={logo} alt="logo" />
+            </Arrow>
+          </Col>
+          <Col xs={10} md={7} lg={5}>
+            <Location>
+              <LocationInput type="text" placeholder="Try “Miami”" />
+            </Location>
+          </Col>
+          <Col lgOffset={2} lg={4}>
+            <Navigation>
+              <Link href="#">Become a host</Link>
+              <Link href="#">Help</Link>
+              <Link href="#">Sign up</Link>
+              <Link href="#">Log in</Link>
+            </Navigation>
+          </Col>
+        </Row>
+      </Grid>
+      <Hr marginTop={16} marginBottom={16} />
+    </Header>
   );
 }
