fe53544f95c72571df15bb63867f9c0a339031d3
Vladimir Lykov <velykov@yandex.ru>
2017-11-11 11:47:05 +0300

    add normalize fix styles

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import Experiences from "./Experiences";
 import Homes from "./Homes";
 import Reservations from "./Reservations";
 import Destinations from "./Destinations";
+import "normalize.css";
 
 const PageWrapper = styled.section`
   font-family: Circular_Air-Light, Helvetica Neue, Helvetica, Arial, sans-serif;
   padding-bottom: 24px;
   color: #383838;
+  max-width: 966px;
+  margin: 0 auto;
 `;
 
 class App extends Component {
