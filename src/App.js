0f5915721f6c22400447e4a477a531a429575879
Vladimir Lykov <velykov@yandex.ru>
2017-11-14 23:13:19 +0300

    refactoring styles

--- src/App.js
+++ src/App.js
@@ -5,33 +5,19 @@ import Main from "./Main";
 import Homes from "./Homes";
 import "normalize.css";
 
-const PageWrapper = styled.section`
+const Style = styled.div`
   font-family: Circular_Air-Light, Helvetica Neue, Helvetica, Arial, sans-serif;
-  padding-bottom: 24px;
   color: #383838;
-  margin: 0 auto;
-
-  @media (min-width: 320px) {
-    max-width: 320px;
-  }
-
-  @media (min-width: 768px) {
-    max-width: 768px;
-  }
-
-  @media (min-width: 966px) {
-    max-width: 966px;
-  }
 `;
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <PageWrapper>
+        <Style>
           <Route path="/" exact component={Main} />
           <Route path="/homes" component={Homes} />
-        </PageWrapper>
+        </Style>
       </BrowserRouter>
     );
   }
