efe828b243d34c7c366e0d4c551a5608f4cd6135
Vladimir Lykov <velykov@yandex.ru>
2017-11-14 23:14:18 +0300

    add content wrapper with fixed width, add card's components, add section, fix styles

--- src/UI/index.js
+++ src/UI/index.js
@@ -1,8 +1,23 @@
 import { Col } from "react-flexbox-grid";
 import styled from "styled-components";
 
+const ContentWrapper = styled.div`
+  margin: 0 auto;
+  @media (min-width: 320px) {
+    width: 320px;
+  }
+
+  @media (min-width: 768px) {
+    width: 768px;
+  }
+
+  @media (min-width: 966px) {
+    width: 966px;
+  }
+`;
+
 const Img = styled.img`
-  object-fit: cover;
+  object-fit: fill;
   height: ${props => (props.height ? props.height : "auto")};
   width: ${props => (props.width ? props.width : "auto")};
 `;
@@ -17,33 +32,39 @@ const Link = styled.a`
   line-height: 24px;
 `;
 
-const Hr = styled.hr`
-  margin-top: ${props => (props.marginTop ? props.marginTop : 0)}px;
-  margin-bottom: ${props => (props.marginBottom ? props.marginBottom : 0)}px;
-  border: 0;
-  height: 1px;
-  background: #484848;
-  opacity: 0.25;
+const Section = styled.section`
+  @media (min-width: 320px) {
+    margin-bottom: 40px;
+  }
+  @media (min-width: 768px) {
+    margin-bottom: 48px;
+  }
 `;
 
 const SectionTitle = styled.h2`
   @media (min-width: 320px) {
-    font: bold 24px/28px "Circular_Air-Bold";
+    font: bold 24px/28px Circular_Air-Bold;
+    margin: 0 0 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
   }
   @media (min-width: 768px) {
-    font: bold 32px/34px "Circular_Air-Bold";
+    font: bold 32px/34px Circular_Air-Bold;
+    margin: 0 0 24px;
   }
 `;
 
 const Reviews = styled.div`
   display: flex;
   flex-direction: row;
-  align-items: center;
+  align-items: bottom;
 `;
 
 const Review = styled.div`
   font: normal 12px/14px Circular_Air-Book;
   text-align: left;
+  paddin-left: 4px;
 `;
 
 const Slider = styled.div`
@@ -63,4 +84,59 @@ const Slide = styled(Col)`
   flex: 0 0 1;
 `;
 
-export { Slider, Slide, Img, Link, Hr, SectionTitle, Reviews, Review };
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: #fff;
+`;
+
+const CardImg = styled(Img)`
+  @media (min-width: 320px) {
+    margin-bottom: 8px;
+  }
+`;
+
+const CardTitle = styled.h4`
+  @media (min-width: 320px) {
+    font: bold 13px/15px Circular_Air-Bold;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+    margin-top: 0;
+    margin-bottom: ${props =>
+      props.marginBottom ? `${props.marginBottom}px` : 0};
+  }
+
+  @media (min-width: 768px) {
+    font: bold 15px/18px Circular_Air-Bold;
+    text-overflow: 0;
+    white-space: 0;
+    overflow: 0;
+  }
+`;
+
+const CardTitleDescription = styled.span`
+  @media (min-width: 320px) {
+    font: normal 13px/15px Circular_Air-Light;
+  }
+
+  @media (min-width: 768px) {
+    font: lighter 15px/18px Circular_Air-Light;
+  }
+`;
+
+export {
+  ContentWrapper,
+  Slider,
+  Slide,
+  Img,
+  Link,
+  Section,
+  SectionTitle,
+  Reviews,
+  Review,
+  Card,
+  CardImg,
+  CardTitle,
+  CardTitleDescription
+};
