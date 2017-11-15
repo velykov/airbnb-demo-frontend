0baaff52aa3ff985a1c882b3b817ceeb2fce0d9a
Vladimir Lykov <velykov@yandex.ru>
2017-11-15 19:23:49 +0300

    change markup for fixed header

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,25 +1,37 @@
 import React from "react";
-import MainHeader from "./MainHeader";
+import Header from "../Header";
 import Footer from "../Footer";
 import Explore from "./Explore";
 import Experiences from "./Experiences";
 import Homes from "./Homes";
 import Reservations from "./Reservations";
 import Destinations from "./Destinations";
-import { ContentWrapper } from "../UI";
+import styled from "styled-components";
+
+const Main = styled.main`
+  margin-top: 120px;
+  @media (min-width: 768px) {
+    margin-top: 128px;
+  }
+  @media (min-width: 992px) {
+    width: 996px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+`;
 
 export default () => {
   return (
-    <ContentWrapper>
-      <MainHeader />
-      <main>
+    <div>
+      <Header />
+      <Main>
         <Explore />
         <Experiences />
         <Homes />
         <Reservations />
         <Destinations />
-      </main>
+      </Main>
       <Footer />
-    </ContentWrapper>
+    </div>
   );
 };
