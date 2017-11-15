fbe0305ab8de4e46616c313d7c0f6bf8b1843c57
Vladimir Lykov <velykov@yandex.ru>
2017-11-15 19:23:16 +0300

    fix styles for fixed header

--- src/Homes/index.js
+++ src/Homes/index.js
@@ -25,108 +25,113 @@ const Wrapper = styled.div`
   }
 `;
 
+const Main = styled.main`
+  margin-top: 160px;
+`;
+
 export default function() {
   return (
-    <ContentWrapper>
+    <div>
       <HomesHeader value="Anywhere · Homes" />
       <Filters />
-
-      <Grid>
-        <Row>
-          <Col xs={12} lg={8}>
-            <main>
-              <Grid>
-                <Row>
-                  <Col xs={12} md={6} lg={4}>
-                    <Wrapper>
-                      <Card
-                        title="La Salentina, see, nature & relax"
-                        img={img1}
-                        alt="La Salentina, see, nature & relax"
-                        price={82}
-                        type="Entire house"
-                        beds="9 beds"
-                        reviews={97}
-                      />
-                    </Wrapper>
-                  </Col>
-                  <Col xs={12} md={6} lg={4}>
-                    <Wrapper>
-                      <Card
-                        title="Your private 3 bedr. riad and exclusi..."
-                        img={img2}
-                        alt="Your private 3 bedr. riad and exclusi..."
-                        price={82}
-                        type="Entire house"
-                        beds="5 beds"
-                        reviews={161}
-                      />
-                    </Wrapper>
-                  </Col>
-                  <Col xs={12} md={6} lg={4}>
-                    <Wrapper>
-                      <Card
-                        title="Dreamy Tropical Tree House"
-                        img={img3}
-                        alt="Dreamy Tropical Tree House"
-                        price={200}
-                        type="Entire treehouse"
-                        beds="1 bed"
-                        reviews={364}
-                        paddingBottom={24}
-                      />
-                    </Wrapper>
-                  </Col>
-                  <Col xs={12} md={6} lg={4}>
-                    <Wrapper>
-                      <Card
-                        title="Best location old town luxury loft"
-                        img={img4}
-                        alt="Best location old town luxury loft"
-                        price={110}
-                        type="Entire apartment"
-                        beds="1 bed"
-                        reviews={369}
-                      />
-                    </Wrapper>
-                  </Col>
-                  <Col xs={12} md={6} lg={4}>
-                    <Wrapper>
-                      <Card
-                        title="Lussuoso. Vista incantevole."
-                        img={img5}
-                        alt="Lussuoso. Vista incantevole."
-                        price={83}
-                        type="Entire apartment"
-                        beds="6 bed"
-                        reviews={105}
-                      />
-                    </Wrapper>
-                  </Col>
-                  <Col xs={12} md={6} lg={4}>
-                    <Wrapper>
-                      <Card
-                        title="In the historical center of Lecce"
-                        img={img6}
-                        alt="In the historical center of Lecce"
-                        price={72}
-                        type="Entire house"
-                        beds="1 bed"
-                        reviews={221}
-                      />
-                    </Wrapper>
-                  </Col>
-                </Row>
-              </Grid>
-            </main>
-            <Paginator />
-            <Footer />
-          </Col>
-          <Col lg={4}>
-            <Map center={{ lat: 47.33333, lng: 13.33333 }} zoom={7} />
-          </Col>
-        </Row>
-      </Grid>
-    </ContentWrapper>
+      <Main>
+        <ContentWrapper>
+          <Grid>
+            <Row>
+              <Col xs={12} lg={8}>
+                <main>
+                  <Grid>
+                    <Row>
+                      <Col xs={12} md={6} lg={4}>
+                        <Wrapper>
+                          <Card
+                            title="La Salentina, see, nature & relax"
+                            img={img1}
+                            alt="La Salentina, see, nature & relax"
+                            price={82}
+                            type="Entire house"
+                            beds="9 beds"
+                            reviews={97}
+                          />
+                        </Wrapper>
+                      </Col>
+                      <Col xs={12} md={6} lg={4}>
+                        <Wrapper>
+                          <Card
+                            title="Your private 3 bedr. riad and exclusi..."
+                            img={img2}
+                            alt="Your private 3 bedr. riad and exclusi..."
+                            price={82}
+                            type="Entire house"
+                            beds="5 beds"
+                            reviews={161}
+                          />
+                        </Wrapper>
+                      </Col>
+                      <Col xs={12} md={6} lg={4}>
+                        <Wrapper>
+                          <Card
+                            title="Dreamy Tropical Tree House"
+                            img={img3}
+                            alt="Dreamy Tropical Tree House"
+                            price={200}
+                            type="Entire treehouse"
+                            beds="1 bed"
+                            reviews={364}
+                            paddingBottom={24}
+                          />
+                        </Wrapper>
+                      </Col>
+                      <Col xs={12} md={6} lg={4}>
+                        <Wrapper>
+                          <Card
+                            title="Best location old town luxury loft"
+                            img={img4}
+                            alt="Best location old town luxury loft"
+                            price={110}
+                            type="Entire apartment"
+                            beds="1 bed"
+                            reviews={369}
+                          />
+                        </Wrapper>
+                      </Col>
+                      <Col xs={12} md={6} lg={4}>
+                        <Wrapper>
+                          <Card
+                            title="Lussuoso. Vista incantevole."
+                            img={img5}
+                            alt="Lussuoso. Vista incantevole."
+                            price={83}
+                            type="Entire apartment"
+                            beds="6 bed"
+                            reviews={105}
+                          />
+                        </Wrapper>
+                      </Col>
+                      <Col xs={12} md={6} lg={4}>
+                        <Wrapper>
+                          <Card
+                            title="In the historical center of Lecce"
+                            img={img6}
+                            alt="In the historical center of Lecce"
+                            price={72}
+                            type="Entire house"
+                            beds="1 bed"
+                            reviews={221}
+                          />
+                        </Wrapper>
+                      </Col>
+                    </Row>
+                  </Grid>
+                </main>
+                <Paginator />
+                <Footer />
+              </Col>
+            </Row>
+          </Grid>
+        </ContentWrapper>
+        <Map center={{ lat: 47.33333, lng: 13.33333 }} zoom={7} />
+      </Main>
+    </div>
   );
 }
