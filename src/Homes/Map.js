067f1fa9d87044c6b8ddb24233cc3aef1035f928
Vladimir Lykov <velykov@yandex.ru>
2017-11-17 12:06:18 +0300

    rename const

--- src/Homes/Map.js
+++ src/Homes/Map.js
@@ -2,7 +2,7 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import styled from "styled-components";
 
-const MAX_LG_FIXED_SIZE = 996;
+const maxLgFixedSize = 996;
 
 const Wrapper = styled.div`
   @media (min-width: 992px) {
@@ -11,8 +11,7 @@ const Wrapper = styled.div`
     right: 0;
     height: calc(100% - 136px);
     width: ${() =>
-      MAX_LG_FIXED_SIZE * 4 / 12 +
-      (window.innerWidth - MAX_LG_FIXED_SIZE) / 2}px;
+      maxLgFixedSize * 4 / 12 + (window.innerWidth - maxLgFixedSize) / 2}px;
   }
 `;
 
