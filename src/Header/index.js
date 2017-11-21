75bb036e2c4d712ea2b2dfbc42a4475eb3e8a5d6
Vladimir Lykov <velykov@yandex.ru>
2017-11-21 10:24:49 +0300

    fix header border

--- src/Header/index.js
+++ src/Header/index.js
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom";
 
 const Header = styled.header`
   padding: 16px 0;
-  box-shadow: 0 0.5px rgba(72, 72, 72, 0.3);
+  border-bottom: 0.5px solid rgba(72, 72, 72, 0.3);
   position: fixed;
   top: 0;
   left: 0;
