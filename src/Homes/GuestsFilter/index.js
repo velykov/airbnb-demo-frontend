fc584416155dd0f63e97b7db10d71ed510d6f18d
Vladimir Lykov <velykov@yandex.ru>
2017-11-23 11:56:41 +0300

    add constants and refactor toggle

--- src/Homes/GuestsFilter/index.js
+++ src/Homes/GuestsFilter/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import GuestFilter from "./GuestFilter";
+import filters from "../constants";
 
 export default class GuestsFilter extends React.Component {
   state = {
@@ -8,12 +9,18 @@ export default class GuestsFilter extends React.Component {
     infants: 0
   };
 
+  toggle = () => {
+    this.props.onToggle(filters.guests);
+  };
+
   render() {
     return (
       <GuestFilter
         adults={this.state.adults}
         childs={this.state.childs}
         infants={this.state.infants}
+        isShow={this.props.isShow}
+        onToggle={this.toggle}
         onApply={state => this.setState(state)}
       />
     );
