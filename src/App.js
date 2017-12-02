471bf2c976df780d6432234d3c76ec6e6badb8a6
Vladimir Lykov <velykov@yandex.ru>
2017-12-02 13:45:09 +0300

    add default font family styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
-import styled from "styled-components";
 import Main from "./Main";
 import Homes from "./Homes";
 import "normalize.css";
-
-const BodyStyles = styled.div`
-  font-family: Circular_Air-Light, Helvetica Neue, Helvetica, Arial, sans-serif;
-  color: #383838;
-`;
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <BodyStyles>
+        <div>
           <Route path="/" exact component={Main} />
           <Route path="/homes" component={Homes} />
-        </BodyStyles>
+        </div>
       </BrowserRouter>
     );
   }
